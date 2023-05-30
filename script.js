f2eebef80c016cf0da1394ee88dd9fbe4f978edb
Shubham Mane <123303343+Shubham2511github@users.noreply.github.com>
2023-05-31 01:26:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function threeSum(arr, target) {
-// write your code here
-  
-}
+def decimal_to_binary(decimal):
+    if decimal == 0:
+        return '0'
+    binary = ''
+    while decimal > 0:
+        binary = str(decimal % 2) + binary
+        decimal //= 2
+    return binary
 
-module.exports = threeSum;
+# Example usage
+decimal_number = int(input("Enter a decimal number: "))
+binary_number = decimal_to_binary(decimal_number)
+print("Binary representation:", binary_number)
